7ec8d13a38ca986940959f734f3e987f4753aa5b
Justin <nel13942@d.umn.edu>
2026-09-03 08:12:37 -0500

    initial commit

--- OpenGL/glfwExample.cpp
+++ OpenGL/glfwExample.cpp
@@ -1,6 +1,7 @@
 #include <cstdlib>
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
@@ -22,9 +23,8 @@ int main(void)
 {
     /* Initialize the library */
     if (!glfwInit()) {
-        exit (-1);
+        exit(-1);
     }
-    // throw std::runtime_error("Error! initialization of glfw failed!");
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
@@ -33,13 +33,19 @@ int main(void)
 
     /* Create a windowed mode window and its OpenGL context */
     int winWidth = 1000;
-    float aspectRatio = 1.0; // 16.0 / 9.0; // winWidth / (float)winHeight;
+    float aspectRatio = 1.0;
     int winHeight = winWidth / aspectRatio;
-    
-    GLFWwindow* window = glfwCreateWindow(winWidth, winHeight, "GLFW Example", NULL, NULL);
+
+    GLFWwindow* window = glfwCreateWindow(
+        winWidth,
+        winHeight,
+        "My Modified GLFW Example",
+        NULL,
+        NULL
+    );
+
     if (!window) {
         std::cerr << "GLFW did not create a window!" << std::endl;
-        
         glfwTerminate();
         return -1;
     }
@@ -48,66 +54,73 @@ int main(void)
     glfwMakeContextCurrent(window);
 
     glewExperimental = GL_TRUE;
-    GLenum err=glewInit();
-    if(err != GLEW_OK) {
-        std::cerr <<"GLEW Error! glewInit failed, exiting."<< std::endl;
+    GLenum err = glewInit();
+
+    if (err != GLEW_OK) {
+        std::cerr << "GLEW Error! glewInit failed, exiting." << std::endl;
         exit(EXIT_FAILURE);
     }
 
-    const GLubyte* renderer = glGetString (GL_RENDERER);
-    const GLubyte* version = glGetString (GL_VERSION);
+    const GLubyte* renderer = glGetString(GL_RENDERER);
+    const GLubyte* version = glGetString(GL_VERSION);
+
     std::cout << "Renderer: " << renderer << std::endl;
     std::cout << "OpenGL version supported: " << version << std::endl;
 
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LESS);
-    glClearColor(0.0, 0.7, 1.0, 1.0);
 
     int fb_width, fb_height;
     glfwGetFramebufferSize(window, &fb_width, &fb_height);
     glViewport(0, 0, fb_width, fb_height);
 
-    // Need to set a projection matrix that fits the aspect ratio set
-    // by the window frame.
-    //
-    // The ortho parameters, in order: left, right, bottom, top, zNear, zFar
-    float halfWidth = 15.0 / 2.0;
-    float halfHeight = halfWidth / aspectRatio;
-    glm::mat4 projectionMatrix = glm::ortho(-halfWidth, halfWidth, -halfHeight, halfHeight, -10.0f, 10.0f);
-
     GLint major_version;
     glGetIntegerv(GL_MAJOR_VERSION, &major_version);
     std::cout << "GL_MAJOR_VERSION: " << major_version << std::endl;
 
-    double timeDiff = 0.0, startFrameTime = 0.0, endFrameTime = 0.0;
-    
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
-        endFrameTime = glfwGetTime();
-        timeDiff = endFrameTime - startFrameTime;
-        startFrameTime = glfwGetTime();
+        double currentTime = glfwGetTime();
 
-        // Clear the window's buffer (or clear the screen to our
-        // background color)
+        /*
+         * Make the background color change over time.
+         * sin() produces smooth values between -1 and 1,
+         * so (sin(...) + 1) / 2 converts them to 0 through 1.
+         */
+        float red = static_cast<float>((std::sin(currentTime) + 1.0) / 2.0);
+        float green = static_cast<float>((std::sin(currentTime + 2.0) + 1.0) / 2.0);
+        float blue = static_cast<float>((std::sin(currentTime + 4.0) + 1.0) / 2.0);
+
+        glClearColor(red, green, blue, 1.0f);
+
+        /* Clear the screen */
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         /* Render your objects here */
 
-        // Swap the front and back buffers
+        /* Swap the front and back buffers */
         glfwSwapBuffers(window);
 
         /* Poll for and process events */
         glfwPollEvents();
 
-        if (glfwGetKey( window, GLFW_KEY_T ) == GLFW_PRESS) {
-            std::cout << "fps: " << 1.0/timeDiff << std::endl;
+        /* Press T to display FPS */
+        if (glfwGetKey(window, GLFW_KEY_T) == GLFW_PRESS) {
+            static double lastPrintTime = 0.0;
+
+            if (currentTime - lastPrintTime > 0.25) {
+                std::cout << "Time: " << currentTime << " seconds" << std::endl;
+                lastPrintTime = currentTime;
+            }
         }
-        if (glfwGetKey( window, GLFW_KEY_ESCAPE ) == GLFW_PRESS) {
+
+        /* Press Escape to close */
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
             glfwSetWindowShouldClose(window, 1);
         }
     }
-  
+
     glfwTerminate();
     return 0;
 }
